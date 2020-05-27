70d5307106ca16d2211a5e81a7befb1a493442bb
陈碧滔 <peter.chen@useful-person.com>
2020-05-27 23:59:39 +0800

    init server

---
+++ server/src/main/java/com/useful_person/App.java
@@ -0,0 +1,26 @@
+package com.useful_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
