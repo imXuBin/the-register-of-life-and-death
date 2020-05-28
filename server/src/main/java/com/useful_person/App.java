a774d6483d7066928647ba9be4cf2050bdf7c1d9
陈碧滔 <peter.chen@useful-person.com>
2020-05-28 22:43:53 +0800

    配置数据源

--- server/src/main/java/com/useful_person/App.java
+++ server/src/main/java/com/useful_person/App.java
@@ -1,18 +1,50 @@
 package com.useful_person;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Hello world!
  *
  */
+@Slf4j
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
+public class App implements CommandLineRunner {
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Override
+  public void run(String... args) throws Exception {
+    showConnection();
+    showData();
+  }
+
+  private void showConnection() throws SQLException {
+    log.info(dataSource.toString());
+    Connection conn = dataSource.getConnection();
+    log.info(conn.toString());
+    conn.close();
+  }
+
+  private void showData() {
+    jdbcTemplate.queryForList("select * from foo").forEach(row -> log.info(row.toString()));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 
 }
---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into foo (id, bar) values (1, 'aaa');
+insert into foo (id, bar) values (2, 'bbb');
